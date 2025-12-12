fd9029f6c33c7416787b5bba408e048860cacdb6
alfimonth <alvy1610@gmail.com>
2025-12-12 10:47:29 +0700

    feat(home): goal input event handler

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  const goalInputHandler = (e) => {
+    setEnteredGoalText(e);
+  };
+
+  const addGoalHandler = () => {
+    console.log("Added Text: ", enteredGoalText);
+    setEnteredGoalText("");
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
+          value={enteredGoalText}
+          onChangeText={goalInputHandler}
           style={styles.textInput}
           placeholder="Your Course Goal"
           placeholderTextColor="#ccc"
         />
-        <Button title="Add Goal" />
+        <Button onPress={addGoalHandler} title="Add Goal" />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of Yoar Goals...</Text>
       </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   appContainer: {
